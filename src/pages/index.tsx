e78bd48d0d578ff67d6c49174b94477c43955771
Vinícius Rodrigues <jairrodriguesmd@gmail.com>
2023-04-15 17:46:11 -0300

    leaflet | opencage geosearch

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,38 @@
 // import Map from '../components/Map';
+import { GetServerSideProps } from 'next';
 import dynamic from 'next/dynamic';
-  const Map = dynamic(() => import('./components/Map/index'), {ssr: false});
+const Map = dynamic(() => import('./components/Map/index'), {ssr: false});
 
-export default function Home() {
-  
+interface Props {
+  apiKey: string;
+}
+
+export default function Home({apiKey}: any) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 w-[900px] h-[900px] mx-auto rounded-lg">
-      <Map />
+      <Map apiKey={apiKey}/>
     </main>
   )
+}
+
+
+export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
+  const url = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/maps';
+
+  try {
+    const res = await fetch(url);
+    const { apiKey } = await res.json();
+    return {
+      props: {
+        apiKey,
+      },
+    };
+  } catch (err) {
+    console.error('Failed to fetch API key:', err); 
+    return {
+      props: {
+        apiKey: '',
+      },
+    };
+  }
 }
