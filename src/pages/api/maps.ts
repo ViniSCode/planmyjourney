8fd411ed94f3e1cce16980c98b77e9aa41a776f7
Vinícius Rodrigues <jairrodriguesmd@gmail.com>
2023-04-18 21:11:55 -0300

    vercel ssr | leaflet ssr error

--- src/pages/api/maps.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-// pages/api/maps.ts
-export default function Handler (req: NextApiRequest, res: NextApiResponse) {
-  const mapsApiKey = process.env.MAPS_API_KEY;
-  res.status(200).json({ apiKey: mapsApiKey });
-};
