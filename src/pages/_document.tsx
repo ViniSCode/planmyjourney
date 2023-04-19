22c804150cc62c8165ada8bda5c276383be4da57
Vinícius Rodrigues <jairrodriguesmd@gmail.com>
2023-04-18 21:07:14 -0300

    vercel ssr | leaflet ssr error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,15 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link 
+        {/* <link 
           rel="stylesheet" 
           href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
           integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
           crossOrigin=""
-        />
-         <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
-          integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
-          crossOrigin=""></script>
+        /> */}
       </Head>
       <body>
         <Main />
