16539738d15cf40c348bd24ab29c8363edec17db
Vinícius Rodrigues <jairrodriguesmd@gmail.com>
2023-04-18 22:07:19 -0300

    vercel ssr | leaflet ssr error

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+         {/* <link 
+          rel="stylesheet" 
+          href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+          integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+          crossOrigin=""
+        /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
