dd1c9e77f02ab0f94fa1feedbd03624ccaa76beb
Vinícius Rodrigues <jairrodriguesmd@gmail.com>
2023-05-31 11:53:39 -0300

    responsive plans filter

--- src/pages/plans/[planId].tsx
+++ src/pages/plans/[planId].tsx
@@ -1,10 +1,12 @@
 import { MobileMenu } from "@/components/Navbar/MobileMenu";
 import { PlansHeader } from "@/components/Navbar/PlansHeader";
+import { DisplayTripPlanImages } from "@/components/Plans/DisplayTripPlanImages";
 import { GetPlanDocument, useGetPlanQuery } from "@/generated/graphql";
 import { client, ssrCache } from "@/lib/urql";
 import { GetServerSideProps } from "next";
-import Image from "next/image";
 import { useRouter } from "next/router";
+import { BsFillHeartFill } from "react-icons/bs";
+import { FiBookmark } from "react-icons/fi";
 
 export default function planId() {
   const router = useRouter();
@@ -25,17 +27,24 @@ export default function planId() {
       <main className="px-6 mt-32 max-w-[1120px] md:mt-16 mx-auto pb-20">
         {data && (
           <div>
-            <h3>days: {data.plan?.days}</h3>
-            <h3>
-              expenses: {data.plan?.expenses.min} to {data.plan?.expenses.max}
-            </h3>
-            <Image
-              src={data?.plan?.images[0]}
-              alt="trip image"
-              width={300}
-              height={300}
-              className="max-w-[300px] max-h-[300px]"
-            />
+            <div className="flex justify-between place-items-baseline">
+              <div className="full">
+                <h2 className="text-2xl truncate">
+                  {data.plan?.location[0].country}, Trip Plan
+                </h2>
+                <div className="mt-1 flex items-center gap-1 text-gray-900 font-medium">
+                  <BsFillHeartFill size={13} />
+                  <span className="text-sm">187</span>
+                </div>
+              </div>
+
+              <div className="mt-1 flex items-center gap-1 text-gray-900 font-medium">
+                <FiBookmark size={18} />
+                <span>Save</span>
+              </div>
+            </div>
+
+            <DisplayTripPlanImages images={data.plan?.images} />
           </div>
         )}
       </main>
