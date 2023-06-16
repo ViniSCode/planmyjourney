34125fff80b23bcd39c8b72791247825bdfd7120
Vinícius Rodrigues <jairrodriguesmd@gmail.com>
2023-06-16 12:34:00 -0300

    filter carousel overflow bugfix :bug:

--- src/components/Plans/PlanFilter.tsx
+++ src/components/Plans/PlanFilter.tsx
@@ -22,36 +22,38 @@ export function PlanFilter() {
   }, []);
 
   return (
-    <motion.div
-      drag="x"
-      dragConstraints={{ right: 0, left: -carouselWidth }}
-      className="mt-10 flex gap-8 carousel:justify-center md:gap-12 items-center md:justify-center select-none"
-      ref={slideRef}
-    >
-      <div className="flex flex-col items-center gap-2 cursor-pointer text-pink-500">
-        <FiMap />
-        <span className="text-xs font-medium">All</span>
-      </div>
+    <motion.div className="overflow-hidden">
+      <motion.div
+        drag="x"
+        dragConstraints={{ right: 0, left: -carouselWidth }}
+        ref={slideRef}
+        className="mt-10 flex gap-8 carousel:justify-center md:gap-12 items-center md:justify-center select-none"
+      >
+        <div className="flex flex-col items-center gap-2 cursor-pointer text-pink-500">
+          <FiMap />
+          <span className="text-xs font-medium">All</span>
+        </div>
 
-      <div className="flex flex-col items-center gap-2 cursor-pointer text-gray-700">
-        <FiHeart />
-        <span className="text-xs font-medium">Popular</span>
-      </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer text-gray-700">
+          <FiHeart />
+          <span className="text-xs font-medium">Popular</span>
+        </div>
 
-      <div className="flex flex-col items-center gap-2 cursor-pointer text-gray-700">
-        <FiDollarSign />
-        <span className="text-xs font-medium">Expenses</span>
-      </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer text-gray-700">
+          <FiDollarSign />
+          <span className="text-xs font-medium">Expenses</span>
+        </div>
 
-      <div className="flex flex-col items-center gap-2 cursor-pointer text-gray-700">
-        <FiPlusSquare />
-        <span className="text-xs font-medium">New</span>
-      </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer text-gray-700">
+          <FiPlusSquare />
+          <span className="text-xs font-medium">New</span>
+        </div>
 
-      <div className="flex flex-col items-center gap-2 cursor-pointer text-gray-700">
-        <FiCalendar />
-        <span className="text-xs font-medium">Days</span>
-      </div>
+        <div className="flex flex-col items-center gap-2 cursor-pointer text-gray-700">
+          <FiCalendar />
+          <span className="text-xs font-medium">Days</span>
+        </div>
+      </motion.div>
     </motion.div>
   );
 }
